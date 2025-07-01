19955015234c90ebb78b72d36c76b918c11e39d6
artthurr3 <gamerxand9@gmail.com>
2025-07-01 08:58:40 -0300

    Add files via upload

---
+++ calculo_de_imc.py
@@ -0,0 +1,18 @@
+nome = input("Qual é o nome do paciente? ")
+peso = float(input(f"Qual é o peso do paciente {nome}? "))
+altura = float(input(f"Qual é a altura do paciente {nome}? "))
+
+IMC = peso / (altura * altura)
+
+if IMC <= 18.5:
+    print("Está abaixo do peso")
+elif IMC <=24.9:
+    print("Peso normal")
+elif IMC <= 29.9:
+    print("Sobrepeso")
+elif IMC <= 34.9:
+    print("Obesidade grau I")
+elif IMC <= 39.9:
+    print("Obesidade grau II")
+else:
+    print("Para de comer xtudo fi😡")
